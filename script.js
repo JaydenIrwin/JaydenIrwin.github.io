0519845aff74b8acda879665a2fdabab12bfd758
Jayden Irwin <me@jaydenirwin.com>
2018-11-15 21:19:55 -0500

    dark bug fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function preloader() {
 		var img1 = new Image();
 		var img2 = new Image();
 		var img3 = new Image();
-		var darkText = window.matchMedia("(prefers-color-scheme: dark)") ? "_dark" : "";
+		var darkText = window.matchMedia("(prefers-color-scheme: dark)").matches ? "_dark" : "";
 		img1.src = "setting_game"+darkText+".jpg";
 		img2.src = "setting_yard"+darkText+".jpg";
 		img3.src = "setting_gym"+darkText+".jpg";
@@ -27,7 +27,7 @@ addLoadEvent(preloader);
 document.getElementById("apps").onscroll = function() {devBG()};
 function devBG() {
 	var appsWidth = document.getElementById("apps").scrollWidth;
-	var darkText = window.matchMedia("(prefers-color-scheme: dark)") ? "_dark" : "";
+	var darkText = window.matchMedia("(prefers-color-scheme: dark)").matches ? "_dark" : "";
     if (document.getElementById("apps").scrollLeft < 0.135 * appsWidth) {
         document.getElementById("developer").style.background = 'url("setting_game'+darkText+'.jpg") no-repeat center center';
     } else if (document.getElementById("apps").scrollLeft < 0.405 * appsWidth) {
