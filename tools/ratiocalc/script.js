a2176af3a013e86d9d02061e13eb086f521842bf
Jayden Irwin <me@jaydenirwin.com>
2018-12-09 14:58:58 -0500

    text and removed "tools"

--- tools/ratiocalc/script.js
+++
@@ -1,7 +0,0 @@
-function calc() {
-	let a = parseFloat(document.getElementById("a").value);
-	let b = parseFloat(document.getElementById("b").value);
-	let c = parseFloat(document.getElementById("c").value);
-	let d = document.getElementById("d");
-	d.value = c * ( b / a );
-}
