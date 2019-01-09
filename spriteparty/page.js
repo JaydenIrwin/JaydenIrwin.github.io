0f746e7249e4fb55d7a539fe214a57f8948a28d9
Jayden Irwin <me@jaydenirwin.com>
2019-01-09 14:08:37 -0500

    fix

--- spriteparty/page.js
+++ spriteparty/page.js
@@ -1,5 +1,5 @@
 window.onscroll = () => {
-  const title = document.querySelector('#navbar');
+  const title = document.getElementById("title");
   const b = 255 - this.scrollY;
   const g = 255 - Math.min(this.scrollY/2, 128);
   title.style.color = "rgb(255,"+g+","+b+")";
