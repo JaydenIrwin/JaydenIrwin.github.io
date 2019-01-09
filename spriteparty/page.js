7400dcdf01666d01f2c1b31005abb8322ccf4a24
Jayden Irwin <me@jaydenirwin.com>
2019-01-09 14:31:27 -0500

    "

--- spriteparty/page.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = () => {
-  const title = document.getElementById("title");
-  const b = 255 - (this.scrollY/2);
-  const g = 255 - Math.min(this.scrollY/4, 128);
-  title.style.color = "rgb(255,"+g+","+b+")";
-};
