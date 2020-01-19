3a4035ce228764e2a1db0d3ca8c877d158e45cb3
Jayden Irwin <me@jaydenirwin.com>
2020-01-18 23:10:33 -0500

    links to space UI page, many fixes

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function preloader() {
 		var img2 = new Image();
 		var img3 = new Image();
 		var img4 = new Image();
+		var img5 = new Image();
 		var darkText = window.matchMedia("(prefers-color-scheme: dark)").matches ? "_dark" : "";
 		img1.src = "backdrop_game"+darkText+".jpg";
 		img2.src = "backdrop_yard"+darkText+".jpg";
 		img3.src = "backdrop_gym"+darkText+".jpg";
 		img4.src = "backdrop_factory"+darkText+".jpg";
+		img5.src = "backdrop_space.jpg";
 	}
 }
 function addLoadEvent(func) {
@@ -30,15 +32,17 @@ document.getElementById("apps").onscroll = function() {devBG()};
 function devBG() {
 	var appsWidth = document.getElementById("apps").scrollWidth;
 	var darkText = window.matchMedia("(prefers-color-scheme: dark)").matches ? "_dark" : "";
-    if (document.getElementById("apps").scrollLeft < 0.1 * appsWidth) {
+    if (document.getElementById("apps").scrollLeft < 0.08 * appsWidth) {
         document.getElementById("developer").style.background = 'url("backdrop_game'+darkText+'.jpg") no-repeat center center';
-    } else if (document.getElementById("apps").scrollLeft < 0.2 * appsWidth) {
+    } else if (document.getElementById("apps").scrollLeft < 0.16 * appsWidth) {
     	document.getElementById("developer").style.background = 'url("backdrop_gym'+darkText+'.jpg") no-repeat center center';
-    } else if (document.getElementById("apps").scrollLeft < 0.41 * appsWidth) {
+    } else if (document.getElementById("apps").scrollLeft < 0.25 * appsWidth) {
+    	document.getElementById("developer").style.background = 'url("backdrop_space.jpg") no-repeat center center';
+    } else if (document.getElementById("apps").scrollLeft < 0.46 * appsWidth) {
         document.getElementById("developer").style.background = 'url("backdrop_yard'+darkText+'.jpg") no-repeat center center';
-    } else if (document.getElementById("apps").scrollLeft < 0.57 * appsWidth) {
+    } else if (document.getElementById("apps").scrollLeft < 0.63 * appsWidth) {
         document.getElementById("developer").style.background = 'url("backdrop_factory'+darkText+'.jpg") no-repeat center center';
-	} else if (document.getElementById("apps").scrollLeft < 0.72 * appsWidth) {
+	} else if (document.getElementById("apps").scrollLeft < 0.77 * appsWidth) {
     	document.getElementById("developer").style.background = 'url("backdrop_gym'+darkText+'.jpg") no-repeat center center';
     } else {
 		document.getElementById("developer").style.background = 'url("backdrop_factory' + darkText + '.jpg") no-repeat center center';
